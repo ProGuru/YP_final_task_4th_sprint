bc9e584c1026547b4d1b14303222d189ac0c4ac8
Ubuntu Server from home <zemsckov.dmitry@yandex.ru>
2025-12-01 00:56:58 +0300

    Write daystep's function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,13 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -13,8 +19,43 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, errors.New("ошибка в процессе деления строки")
+	}
+
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка при конвертации строки в число: %v", err)
+	}
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов меньше или равно 0: %v", err)
+	}
+
+	strollTime, err := time.Parse("15h04m", parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка при преобразовании строки в длительность прогулки: %v", err)
+	}
+	strollDuration := time.Duration(strollTime.Hour() + strollTime.Minute())
+
+	return steps, strollDuration, nil
 }
 
+// DayActionInfo должна парсить строку с данными с помощью parsePackage(),
+// вычислять дистанцию в километрах и количество потраченных калорий и возвращать строку
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	steps, strollDuration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	if steps <= 0 {
+		return ""
+	}
+
+	distanceInM := steps * stepLength
+	distanceInKm := distanceInM / mInKm
+	burntCalories := spentcalories.WalkingSpentCalories(steps, weight, height, strollDuration)
+	return fmt.Sprintf("Количество шагов: %d./nДистанция составила %.2f км./nВы сожгли %.2f ккал.", steps, distanceInKm, burntCalories)
 }
